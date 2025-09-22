cd80a2eac6cb8cda6ac4c7c5dd9779e228073e37
VanyBany <148676563+VanyBany@users.noreply.github.com>
2025-09-22 13:38:57 +0500

    Add files via upload

---
+++ 22.09/Password.py
@@ -0,0 +1,16 @@
+from string import *
+from random import *
+
+def pd():
+    letters = "".join(choice(ascii_uppercase) for i in range(3))
+    
+    chisla = "".join(choice(digits) for i in range(3))
+
+    symbols = "".join(choice(punctuation) for i in range(2))
+    
+    password =  letters + chisla + symbols
+
+    return password
+
+print(pd())
+
