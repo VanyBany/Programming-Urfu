addd200b2defe9c9d664c6e3a89684dd1a763cca
VanyBany <148676563+VanyBany@users.noreply.github.com>
2025-10-05 14:23:42 +0500

    Add files via upload

--- 29.09(def)/rdpass.py
+++ 29.09(def)/rdpass.py
@@ -4,8 +4,12 @@ from random import *
 
 
 def rdpass(l,u,symb,dg,ln):
-    settings = [l,u,symb,dg]
+    settings = [l,u,symb,dg,ln]
+    if ln < 8:
+        return "Пароль должен быть не менее 8 символов!"
     letters = ""
+
+    # Проверка настроек
     for i in range(len(settings)):
         if settings[i] == 1:
             if i == 0:
@@ -16,15 +20,39 @@ def rdpass(l,u,symb,dg,ln):
                 letters += punctuation
             elif i == 3:
                 letters += digits
-    pd = "".join(choice(letters) for i in range(int(ln)))
-    return pd
+    if not(any(settings[:-1])):
+        return ("Нет ни одного типа данных")
 
+    # Добавление каждого элемента из настроек
+    else:
+        pd = []
+        if settings[0]:
+            pd.append(choice(ascii_lowercase + "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"))
+        if settings[1]:
+            pd.append(choice(ascii_uppercase + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"))
+        if settings[2]:
+            pd.append(choice(punctuation))
+        if settings[3]:
+            pd.append(choice(digits))
+    # Добавление оставшихся элементов
+        rem_ln = ln - len(pd)
+        if rem_ln > 0:
+            for i in range(rem_ln):
+                pd.append(choice(letters))
+        shuffle(pd)
+        pd = "".join(pd)
+        return pd
+
+
+
+
+# Ввод настроек пароля
 print("Введите параметры:")
-l = input("Наличие верхнего регистра(1 или 0): ")
-u = input("Наличие нижнего регистра: ")
-symb = input("Наличие специальных символов: ")
-dg = input("Наличие цифр: ")
-ln = input("Длина пароля: ")
+l = int(input("Наличие нижнего регистра(1 или 0): ").strip())
+u = int(input("Наличие верхнего регистра: ").strip())
+symb = int(input("Наличие специальных символов: ").strip())
+dg = int(input("Наличие цифр: ").strip())
+ln = int(input("Длина пароля(минимум 8 символов): ").strip())
 
 pd = rdpass(l,u,symb,dg,ln)
 print(pd)
